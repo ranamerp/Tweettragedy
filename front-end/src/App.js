2af3d558e33015508e3a1003f9443e473584b4be
Kadamitas <agmlodkowski@gmail.com>
2020-11-29 11:49:57 -0600

    front end update
    front end update

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
-import Main from './components/Main'
+import NavSearchBar from './components/NavSearchBar'
+
 
 function App() {
+
   return (
-    <Main/>
+    <NavSearchBar/>
   );
 }
 
