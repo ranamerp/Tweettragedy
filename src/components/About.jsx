a3d61dd5daca3253996ba5b6342007ce77a2fb52
Simon Markus <52682568+Markusovich@users.noreply.github.com>
2020-12-05 22:37:46 -0600

    Add files via upload

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -9,7 +9,7 @@ const About = () => {
             
             <Navbar bg="primary" variant="dark" style={{width:"100vw", height: "10vh"}}>  
             <br></br>
-                <Navbar.Brand href="#home"> &nbsp;&nbsp; Tweetragety </Navbar.Brand>
+                <Navbar.Brand href="#home"> &nbsp;&nbsp; Tweettragedy </Navbar.Brand>
                 <Nav className="mr-auto">
                         <Nav.Link as={Link} to="./">Home</Nav.Link>
                         <Nav.Link as={Link} to="./About">About</Nav.Link>
@@ -29,15 +29,15 @@ const About = () => {
             <br></br>
             <center>
                 <Jumbotron style={{width:"60vw",height:"60vh" , textAlign:"left", fontSize:"3vh"}}>
-                    Dedicated to our heroes in the Fire Department.
+                    Dedicated to first responders and humanitarians 
+                    <br></br>
                     <br></br>
 
                     Created as a low latency service to alert first responders as well as the general population to give them information on disasters.
-                    Group of SIU students made this website using Machine Learning, Graphing, Mapping, and Databases. This is created to illustrate the amount of reported tweets about a given disaster such that you could ascertain whether a disaster is occurring and how often they occur within a given timeframe, as well as how often they occur throughout a given season.
+                    A group of SIU students made this website using Machine Learning, Graphing, Mapping, and Databases. This is created to illustrate the amount of reported tweets about a given disaster such that you could ascertain whether a disaster is occurring and how often they occur within a given timeframe, as well as how often they occur throughout a given season.
                     <br></br>
-                    Brought to you by, 
                     <br></br>
-                    Our Team, and Our School SIU! GO SALUKIS!!!
+                    Brought to you by Simon M., Prem R., Claire F., Matt C., Aron M. and most importantly Southern Illinois University.
 
                 </Jumbotron>
             </center>
--- src/components/Sourcing.jsx
+++ src/components/Sourcing.jsx
@@ -9,7 +9,7 @@ const Sourcing = () => {
                 
         <header style={{width:"100vw", height: "10vh"}}>
             <Navbar bg="primary" variant="dark" style={{width:"100vw", height: "10vh"}}>  
-                <Navbar.Brand href="#home"> &nbsp;&nbsp; Tweetragety </Navbar.Brand>
+                <Navbar.Brand href="#home"> &nbsp;&nbsp; Tweettragedy </Navbar.Brand>
                 <Nav className="mr-auto">
                         <Nav.Link as={Link} to="./#">Home</Nav.Link>
                         <Nav.Link as={Link} to="./#/About">About</Nav.Link>
